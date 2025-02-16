97423e53a30ad01a4a8a665cf68fdecdd31f8a24
AnastasiiaBochkareva <120329458+AnastasiiaBochkareva@users.noreply.github.com>
2025-02-16 22:06:08 +0400

    delite extra mixins form LinguaSphere site

--- src/sites/LinguaSphere/index.ts
+++ src/sites/LinguaSphere/index.ts
@@ -4,6 +4,3 @@ import "@/components/slider/index";
 import "@/components/counter/index";
 
 import "@/components/header-registration/index";
-// import "@/sites/LinguaSphere/components/reveal/index";
-
-import "@/sites/LinguaSphere/components/registration/index";
