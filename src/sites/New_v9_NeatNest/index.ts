09d617593e54fd164874edb744809dfb2661d6e3
AnastasiiaBochkareva <120329458+AnastasiiaBochkareva@users.noreply.github.com>
2025-02-16 20:37:37 +0400

    delite slider from site/components, add slider from global components to NeatNest site

--- src/sites/New_v9_NeatNest/index.ts
+++ src/sites/New_v9_NeatNest/index.ts
@@ -1,6 +1,6 @@
 import "@/components/burger/index";
+import "@/components/slider/index";
 import "@/sites/New_v9_NeatNest/components/faq/index";
 import "@/sites/New_v9_NeatNest/components/reveal/index";
-import "@/sites/New_v9_NeatNest/components/catalog-block/index";
 import "@/sites/New_v9_NeatNest/components/delivery/index";
 import "@/sites/New_v9_NeatNest/components/tips/index";
