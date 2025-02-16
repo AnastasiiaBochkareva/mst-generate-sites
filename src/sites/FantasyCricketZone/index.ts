ec9718153848ee3d626ec68a09c53bb5fc8018fa
AnastasiiaBochkareva <120329458+AnastasiiaBochkareva@users.noreply.github.com>
2025-02-16 20:35:29 +0400

    created FantasyCricketZone site

---
+++ src/sites/FantasyCricketZone/index.ts
@@ -0,0 +1,4 @@
+import "@/components/burger/index";
+import "@/components/slider/index";
+import "@/sites/FantasyCricketZone/components/reveal/index";
+import "@/sites/FantasyCricketZone/components/faq/index";
